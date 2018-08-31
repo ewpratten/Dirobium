8153c3f83f6344790d0277c17b6fbbce84d8d589
Evan Pratten <ewpratten@gmail.com>
2018-08-31 12:28:12 -0400

    addition and subtraction

--- src/execution.py
+++ src/execution.py
@@ -1,73 +1,16 @@
-import shareddata as globl
-import system as system
-import registers as registers
-
-def mov(params):
-	frm = [params[1], params[2]]
-	to = [params[3], params[4]]
-	
-	#check type of frm and turn back to int
-	if frm[0] == "00000001":
-		frm = int(frm[1], 2)
-	elif frm[0] == "000000010":
-		frm = registers.general[int(frm[1], 2)]
-	elif frm[0] == "00000011":
-		print("rom")
-	elif frm[0] == "00000100":
-		print("boot")
-	elif frm[0] == "00000101":
-		frm = registers.extended[int(frm[1], 2)]
-	else:
-		system.coredump()
-		exit(1)
-	
-	# check type of to and store frm in location
-	if to[0] == "00000010":
-		registers.general[int(to[1], 2)] = frm
-	elif to[0] == "00000101":
-		registers.extended[int(to[1], 2)] = frm
-	else:
-		system.coredump()
-		exit(1)
-
-def add():
-	print("add")
-
-def sub():
-	print("sub")
-
-def mul():
-	print("mul")
-
-def div():
-	print("div")
-
-def ret():
-	print("ret")
-
-def call():
-	print("call")
-
-def jmpf():
-	print("jmpf")
-
-def jmpb():
-	print("jmpb")
-
-def jmp():
-	print("jmp")
+import instructions as ins
 
 execute = {
-	"10000001":mov,
-	"10000010":add,
-	"10000011":sub,
-	"10000100":mul,
-	"10000101":div,
-	"10000110":ret,
-	"10000111":call,
-	"10001000":jmpf,
-	"10001001":jmpb,
-	"10001011":jmp
+	"10000001":ins.mov,
+	"10000010":ins.add,
+	"10000011":ins.sub,
+	"10000100":ins.mul,
+	"10000101":ins.div,
+	"10000110":ins.ret,
+	"10000111":ins.call,
+	"10001000":ins.jmpf,
+	"10001001":ins.jmpb,
+	"10001011":ins.jmp
 }
 
 def run(code):
---
+++ src/instructions.py
@@ -0,0 +1,84 @@
+import shareddata as globl
+import system as system
+import registers as registers
+
+def decode(frm):
+	#check type of frm and turn back to int
+	if frm[0] == "00000001":
+		frm = int(frm[1], 2)
+	elif frm[0] == "000000010":
+		frm = registers.general[int(frm[1], 2)]
+	elif frm[0] == "00000011":
+		print("rom")
+	elif frm[0] == "00000100":
+		print("boot")
+	elif frm[0] == "00000101":
+		frm = registers.extended[int(frm[1], 2)]
+	else:
+		system.coredump()
+		exit(1)
+	return frm
+
+def decodeTo(to, data):
+	# check type of to and store data in location
+	if to[0] == "00000010":
+		registers.general[int(to[1], 2)] = data
+	elif to[0] == "00000101":
+		registers.extended[int(to[1], 2)] = data
+	else:
+		system.coredump()
+		exit(1)
+	
+def mov(params):
+	frm = [params[1], params[2]]
+	to = [params[3], params[4]]
+	
+	frm = decode(frm)
+	decodeTo(to, frm)
+	
+	
+
+def add(params):
+	v1 = [params[1], params[2]]
+	v2 = [params[3], params[4]]
+	to = [params[5], params[6]]
+	
+	v1 = decode(v1)
+	v2 = decode(v2)
+	
+	val_sum = v1 + v2
+	
+	decodeTo(to, val_sum)
+
+def sub(params):
+	v1 = [params[1], params[2]]
+	v2 = [params[3], params[4]]
+	to = [params[5], params[6]]
+	
+	v1 = decode(v1)
+	v2 = decode(v2)
+	
+	val_sum = v1 - v2
+	
+	decodeTo(to, val_sum)
+
+def mul(params):
+	print("mul")
+
+def div(params):
+	print("div")
+
+def ret(params):
+	print("ret")
+
+def call(params):
+	print("call")
+
+def jmpf(params):
+	print("jmpf")
+
+def jmpb(params):
+	print("jmpb")
+
+def jmp(params):
+	print("jmp")
